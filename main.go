14e0d7b039103f1e1f7c1d1cd1690768bd887c54
Dmitriy Zinin <admin@kami-no.ru>
2019-10-10 17:10:52 +0300

    graceful shutdown
    https://getgophish.com/blog/post/2018-12-02-building-web-servers-in-go/
    
    still not clear, further research required

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +27,17 @@ func main() {
 		// TLSConfig: m.TLSConfig(),
 	}
 	// log.Fatal(server.ListenAndServeTLS("", ""))
+	go func() {
+		log.Fatal(server.ListenAndServe())
+	}()
 
-	log.Fatal(server.ListenAndServe())
+	// Wait for an interrupt
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	// Attempt a graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_ = server.Shutdown(ctx)
 }
